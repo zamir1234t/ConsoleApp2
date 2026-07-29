545eea1701bb31486f271a6d0f6f92d2442bba2b
zamir1234t <firamogurova579@gmail.com>
2026-07-29 18:08:52 +0400

    test

---
+++ ConsoleApp2/newFolder/newFolders/eliment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory.newFolder.newFolders.ConsoleApp2
+{
+    internal class eliment
+    {
+        
+        public static void Run(string[] args)
+        {
+            string[] v = "the first line".Split('\n');
+            StringBuilder a = new();
+            a.AppendLine("the first line");
+            foreach( var i in v)
+            {
+                a.AppendLine(i);
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
