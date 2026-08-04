616a564ee62c9da39bc961ebc071695866be59ea
zamir1234t <firamogurova579@gmail.com>
2026-08-04 19:36:31 +0400

    test

---
+++ ConsoleApp2/newFolder/newFolders/Getcommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commands.newFolder.newFolders.ConsoleApp2
+{
+ class Getcommand
+    {
+        class randomNumber
+        {
+            private static Random random;
+
+            static randomNumber()
+            {
+                random = new Random();
+            }
+            public int get => random.Next();
+        }
+        public static void Run()
+        {
+            randomNumber random = new();
+           while (true)
+            {
+               Console.WriteLine( random.get);
+            }
+        }
+    }
+}
