e9948cdc59bf99ed1f7161f0a05d8ad6030114f9
zamir1234t <firamogurova579@gmail.com>
2026-08-07 19:32:08 +0400

    Fixed

--- ConsoleApp2/newFolder/newFolders/small.cs
+++ ConsoleApp2/newFolder/newFolders/small.cs
@@ -13,14 +13,14 @@ namespace mallenkiy.newFolder.newFolders.ConsoleApp2
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public byte age { get; set; }
-            public string fullname => $"{FirstName} {LastName}";
+            public string fullname => $"{FirstName} {LastName} {age}";
 
-            public string invalid => $"{FirstName}";
+            public string invalid => $"{FirstName} {LastName} {age}";
         }
 
         class classic2: classic
         {
-                       public string invalid2 => $"{LastName}";
+                       public string invalid2 => $"{LastName}{FirstName} {age}";
         }
 
         public static void Run()
@@ -31,7 +31,7 @@ namespace mallenkiy.newFolder.newFolders.ConsoleApp2
                 LastName = "Doe",
                 age = 10
             };
-            Console.WriteLine($"Full {classices2.LastName} {classices2.FirstName} {classices2.age}");
+            Console.WriteLine($"Full {classices2.fullname}");
         }
     }
 }
