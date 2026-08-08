57a0c661445cc9e30130818926b7c807dbc29562
zamir1234t <firamogurova579@gmail.com>
2026-08-08 20:12:00 +0400

    newFile

---
+++ ConsoleApp2/newFolder/newFolders/taskets/start.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staets.newFolder.newFolders.taskets.ConsoleApp2
+{
+  class Start
+    {
+        class Person
+        {
+            public string Name { get; set; }
+
+            public string Introduce() => $"Hi, I'm {Name}.";
+        }
+
+        class Employee : Person
+        {
+            public string JobTitle { get; set; }
+
+            public new string Introduce() => $"Hi, I'm {Name}. I'm a {JobTitle}.";
+        }
+        public static void Run()
+        {
+            var employee = new Employee { Name = "John", JobTitle = "Manager" };
+            Console.WriteLine(employee.Introduce());
+        }
+    }
+}
