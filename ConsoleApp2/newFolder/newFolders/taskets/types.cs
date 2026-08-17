00f05594939518c03d68ff70b71a579aac7a1433
zamir1234t <firamogurova579@gmail.com>
2026-08-17 17:38:52 +0400

    types

---
+++ ConsoleApp2/newFolder/newFolders/taskets/types.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nameTypes.newFolder.newFolders.taskets.ConsoleApp2
+{
+class types
+    {
+       struct optionens
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+
+            public readonly double distance(Point other)
+            {
+                var dx = X - other.X;
+                var dy = Y - other.Y;
+                return Math.Sqrt(dx * dx + dy * dy);
+            }
+            public override  string  ToString() => $"{X}, {Y}";
+        }
+        public static void Run()
+        {
+            var opm = new  optionens { X = 1.391, Y = 2.123 };
+            var p1 = opm;
+            p1.X = 2.121;
+            Console.WriteLine(p1);
+            Console.WriteLine(p1.X);
+        }
+    }
+}
