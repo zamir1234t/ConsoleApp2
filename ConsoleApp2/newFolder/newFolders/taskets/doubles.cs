69db72f4c902f43b266252d7c40bda66d92f4d91
zamir1234t <firamogurova579@gmail.com>
2026-08-17 19:45:56 +0400

    temp

---
+++ ConsoleApp2/newFolder/newFolders/taskets/doubles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dol.newFolder.newFolders.taskets.ConsoleApp2
+{
+ class doubles
+    {
+        readonly struct temperature
+        {
+            public double Celsius { get; }
+
+            public temperature(double celsius) => Celsius = celsius;
+
+            public double Fahrenheit => Celsius * 1.8 + 32;
+
+            public override string ToString()
+            {
+                return $"{Celsius:F1}C {Fahrenheit:F1}F";
+            }
+        }
+        public static void Run()
+        {
+            var temp = new temperature(20);
+            Console.WriteLine(temp);
+        }
+    }
+}
